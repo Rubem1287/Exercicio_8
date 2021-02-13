f5ff2732d6bad954caa54c942f669b142c44cedc
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-13 18:51:02 -0300

    Add files via upload

---
+++ Exercício 8/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+namespace Exercício_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma, menor, maior, media, f, m;
+            soma = 0;
+            menor = 0;
+            maior = 0;
+            media = 0;
+            f = 0;
+            m = 0;
+            double[] altura;
+            char[] genero;
+
+            int n = int.Parse(Console.ReadLine());
+
+            altura = new double[n];
+            genero = new char[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vetor = Console.ReadLine().Split(' ');
+                altura[i] = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+                genero[i] = char.Parse(vetor[1]);
+
+                if (genero[i] == 'F')
+                {
+                    soma += altura[i];
+                    f++;
+                }
+                else
+                {
+                    m++;
+                }
+
+                if (menor == 0)
+                {
+                    menor = altura[i];
+                }
+                else if (menor > altura[i])
+                {
+                    menor = altura[i];
+                }
+
+                if (maior == 0)
+                {
+                    maior = altura[i];
+                }
+                else if (maior < altura[i])
+                {
+                    maior = altura[i];
+                }
+
+            }
+
+            media = soma / f;
+
+            Console.WriteLine();
+            Console.WriteLine("Menor altura = " + menor.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.WriteLine("Maior altura = " + maior.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.WriteLine("Altura média das mulheres = " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.WriteLine("Numero de Homens = " + m);
+        }
+    }
+}
